7161a4119e0726f1f5888a19f30d5fd8a5bcdc73
adobrovolsky <manowartop97@gmail.com>
2023-05-18 20:38:51 +0300

    Updated AboutController

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -13,7 +13,7 @@ class AboutController extends Controller
      */
     public function index(CategoryServiceInterface $categoryService): View
     {
-        echo 345;
+        echo 345567;
         return view('about.about', [
             'categories' => $categoryService->getAll(),
         ]);
