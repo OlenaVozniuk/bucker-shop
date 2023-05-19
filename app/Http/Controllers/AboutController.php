1a2172ed6623263bf25368529f26d7f75bd652cc
OlenaVozniuk <elena.voznuk@gmail.com>
2023-05-19 10:15:26 +0300

    Update

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -13,7 +13,6 @@ class AboutController extends Controller
      */
     public function index(CategoryServiceInterface $categoryService): View
     {
-        echo 3455678798;
         return view('about.about', [
             'categories' => $categoryService->getAll(),
         ]);
