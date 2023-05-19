c434e61cb5930b9a9c8570ad52d9d53bdbd6a138
OlenaVozniuk <elena.voznuk@gmail.com>
2023-05-19 11:01:36 +0300

    Changes in task_1

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -13,7 +13,7 @@ class AboutController extends Controller
      */
     public function index(CategoryServiceInterface $categoryService): View
     {
-        echo 3456;
+        echo 34456;
         return view('about.about', [
             'categories' => $categoryService->getAll(),
         ]);
