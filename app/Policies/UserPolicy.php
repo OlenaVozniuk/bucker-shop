e8cd5787574a1a941a44d8a279f0456249a5ab02
OlenaVozniuk <elena.voznuk@gmail.com>
2023-05-21 18:33:43 +0300

    UserPolicy finally

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -17,10 +17,6 @@ class UserPolicy
      */
     public function manage(User $userActive, User $userToChange): bool
     {
-        echo 596977;
-        echo 14566;
-        echo 1445;
-        echo 5678;
         return $userActive->getKey() !== $userToChange->getKey();
     }
 }
